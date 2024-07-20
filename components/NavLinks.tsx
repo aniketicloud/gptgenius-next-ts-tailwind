50a0ad2740d3c6d15faf86b0eea85d5eb0449f9e
Aniket Pandharabale <aniketicloud@gmail.com>
2024-07-20 16:20:14 +0530

    feat: add nav links in sidebar

--- components/NavLinks.tsx
+++ components/NavLinks.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+
+const links: { href: string; label: string }[] = [
+  { href: "/chat", label: "chat" },
+  { href: "/tours", label: "tours" },
+  { href: "/tours/new-tour", label: "new tour" },
+  { href: "/profile", label: "profile" },
+];
 
 const NavLinks = () => {
   return (
-    <div>NavLinks</div>
-  )
-}
+    <ul className="menu text-base-content">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link href={link.href} className="capitalize">
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-export default NavLinks
+export default NavLinks;
